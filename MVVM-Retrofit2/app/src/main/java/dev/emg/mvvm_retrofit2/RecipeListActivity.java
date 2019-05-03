dbcdf1f5b10294073d96015d381e018b4449d379
Edgar Meruvia <emg.appdev@gmail.com>
2019-05-03 11:38:52 -0400

    Test API callback implemented

--- MVVM-Retrofit2/app/src/main/java/dev/emg/mvvm_retrofit2/RecipeListActivity.java
+++ MVVM-Retrofit2/app/src/main/java/dev/emg/mvvm_retrofit2/RecipeListActivity.java
@@ -1,9 +1,23 @@
 package dev.emg.mvvm_retrofit2;
 
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
+import dev.emg.mvvm_retrofit2.model.Recipe;
+import dev.emg.mvvm_retrofit2.requests.RecipeApi;
+import dev.emg.mvvm_retrofit2.requests.ServiceGenerator;
+import dev.emg.mvvm_retrofit2.requests.responses.RecipeSearchResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RecipeListActivity extends BaseActivity {
+
+  private static final String TAG = "RecipeListActivity";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +27,36 @@ public class RecipeListActivity extends BaseActivity {
     findViewById(R.id.test_btn).setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        if (mProgressBar.getVisibility() == View.VISIBLE) {
-          showProgressBar(false);
+        testRetrofitRequest();
+      }
+    });
+  }
+
+  private void testRetrofitRequest() {
+    RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
+
+    Call<RecipeSearchResponse> responseCall = recipeApi.searchRecipe(
+        getResources().getString(R.string.RECIPE_API_KEY),
+        "chicken breast",
+        "1");
+
+    responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+      @Override public void onResponse(Call<RecipeSearchResponse> call,
+          Response<RecipeSearchResponse> response) {
+        Log.d(TAG, "onResponse: server response: " + response.toString());
+        if (response.code() == 200) {
+          Log.d(TAG, "onResponse: response code 200");
+          List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+          for (Recipe recipe : recipes) {
+            Log.d(TAG, "onResponse: " + recipe.getTitle());
+          }
         } else {
-          showProgressBar(true);
+          Log.d(TAG, "onResponse: " + response.errorBody().toString());
         }
+      }
+
+      @Override public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+
       }
     });
   }
