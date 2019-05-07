068b514d434aef2ade3cfe53e9f8dadda1e1fe6d
Edgar Meruvia <emeruvia@gmail.com>
2019-05-07 00:25:53 -0400

    Implement executors for API calls

--- MVVM-Retrofit2/app/src/main/java/dev/emg/mvvm_retrofit2/requests/RecipeApiClient.java
+++ MVVM-Retrofit2/app/src/main/java/dev/emg/mvvm_retrofit2/requests/RecipeApiClient.java
@@ -2,8 +2,13 @@ package dev.emg.mvvm_retrofit2.requests;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import dev.emg.mvvm_retrofit2.AppExecutors;
 import dev.emg.mvvm_retrofit2.model.Recipe;
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static dev.emg.mvvm_retrofit2.util.Constants.NETWORK_TIMEOUT;
 
 /**
  * Created by emeruvia on 5/6/2019.
@@ -26,5 +31,22 @@ public class RecipeApiClient {
 
   public LiveData<List<Recipe>> getRecipes() {
     return mRecipes;
+  }
+
+  public void searchRecipesApi() {
+    final Future handler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
+      @Override public void run() {
+        // retrieve data from REST API
+        //mRecipes.postValue();
+      }
+    });
+
+    AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+      @Override public void run() {
+        // let the user know its timed out
+        // interrupts background thread from making a request if the timeout occurs
+        handler.cancel(true);
+      }
+    }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
   }
 }
