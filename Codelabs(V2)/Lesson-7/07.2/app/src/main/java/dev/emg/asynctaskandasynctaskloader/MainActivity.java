022f2023ce3a92f8aeb2cb018ebc8ef388d6b28a
Edgar Meruvia <emeruvia@gmail.com>
2019-06-19 00:39:03 -0400

    Codelab 07.2 Task 3 finished

--- Codelabs(V2)/Lesson-7/07.2/app/src/main/java/dev/emg/asynctaskandasynctaskloader/MainActivity.java
+++ Codelabs(V2)/Lesson-7/07.2/app/src/main/java/dev/emg/asynctaskandasynctaskloader/MainActivity.java
@@ -1,28 +1,57 @@
 package dev.emg.asynctaskandasynctaskloader;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mBookInput;
-    private TextView mTitleText;
-    private TextView mAuthorText;
+  private EditText mBookInput;
+  private TextView mTitleText;
+  private TextView mAuthorText;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mBookInput = findViewById(R.id.editText_book);
-        mTitleText = findViewById(R.id.titleText);
-        mAuthorText = findViewById(R.id.authorText);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    mBookInput = findViewById(R.id.editText_book);
+    mTitleText = findViewById(R.id.titleText);
+    mAuthorText = findViewById(R.id.authorText);
+  }
 
-    public void searchButton(View view) {
-        String queryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+  public void searchButton(View view) {
+    String queryString = mBookInput.getText().toString();
+    hideKeyboard(view);
+    ConnectivityManager connectivityManager =
+        (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo networkInfo = null;
+    if (connectivityManager != null) {
+      networkInfo = connectivityManager.getActiveNetworkInfo();
     }
+    if (networkInfo != null && networkInfo.isConnected()
+        && queryString.length() != 0) {
+      new FetchBook(mTitleText, mAuthorText).execute(queryString);
+      mAuthorText.setText("");
+      mTitleText.setText(R.string.loading);
+    } else {
+      mAuthorText.setText("");
+      mTitleText.setText((queryString.length() == 0)
+          ? R.string.no_search_term : R.string.no_network);
+    }
+  }
+
+  private void hideKeyboard(View view) {
+    InputMethodManager inputMethodManager =
+        (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    if (inputMethodManager != null) {
+      inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),
+          InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+  }
 }
