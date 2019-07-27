4f6458596484bc7a7bf4101d0abdd4e93ff746b2
Edgar Meruvia <emeruvia@gmail.com>
2019-07-27 19:24:31 -0400

    Lesson 1.3 finished

--- Kotlin Fundamentals/Lesson1/app/src/main/java/dev/emg/lesson1/MainActivity.kt
+++ Kotlin Fundamentals/Lesson1/app/src/main/java/dev/emg/lesson1/MainActivity.kt
@@ -3,38 +3,33 @@ package dev.emg.lesson1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var resultText: TextView
+  private lateinit var diceImage: ImageView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    resultText = findViewById(R.id.result_tv)
+    diceImage = findViewById(R.id.dice_iv)
 
     val rollButton: Button = findViewById(R.id.roll_btn)
     rollButton.setOnClickListener { rollDice() }
 
-    val countUpButton: Button = findViewById(R.id.countUp_btn)
-    countUpButton.setOnClickListener { countUp() }
   }
 
   private fun rollDice() {
-    val randomInt = Random().nextInt(6) + 1
-    resultText.text = randomInt.toString()
-    Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT)
-        .show()
+    val drawableResource = when (Random().nextInt(6) + 1) {
+      1 -> R.drawable.dice_1
+      2 -> R.drawable.dice_2
+      3 -> R.drawable.dice_3
+      4 -> R.drawable.dice_4
+      5 -> R.drawable.dice_5
+      else -> R.drawable.dice_6
+    }
+    diceImage.setImageResource(drawableResource)
   }
 
-  private fun countUp() {
-    when (val currentValue = resultText.text.toString()) {
-      "Hello World!" -> resultText.text = "1"
-      "6" -> return
-      else -> resultText.text = (currentValue.toInt() + 1).toString()
-    }
-  }
 }
