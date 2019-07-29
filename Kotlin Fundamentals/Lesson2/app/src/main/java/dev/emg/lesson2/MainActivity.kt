9b473b6f8fca9cd17ecf47c44b609fa5a26867a9
Edgar Meruvia <emeruvia@gmail.com>
2019-07-29 00:18:21 -0400

    Finished kotlin codelabs Lesson 2: Layouts

--- Kotlin Fundamentals/Lesson2/app/src/main/java/dev/emg/lesson2/MainActivity.kt
+++ Kotlin Fundamentals/Lesson2/app/src/main/java/dev/emg/lesson2/MainActivity.kt
@@ -5,45 +5,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import dev.emg.lesson2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var binding: ActivityMainBinding
+  private val myName: MyName = MyName("EMG")
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    val doneButton: Button = findViewById(R.id.done_button)
-    doneButton.setOnClickListener {
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    binding.myName = myName
+    binding.doneButton.setOnClickListener {
       addNickName(it)
     }
-    findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+    binding.nicknameText.setOnClickListener {
       updateNickname(it)
     }
   }
 
   private fun addNickName(view: View) {
-    val editText = findViewById<EditText>(R.id.nickname_edit)
-    val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-    nicknameTextView.text = editText.text
-    nicknameTextView.visibility = View.VISIBLE
-    editText.visibility = View.GONE
+    binding.apply {
+      myName?.nickname = nicknameEdit.text.toString()
+      invalidateAll()
+      nicknameEdit.visibility = View.GONE
+      doneButton.visibility = View.GONE
+      nicknameText.visibility = View.VISIBLE
+    }
     view.visibility = View.GONE
     // Hide the keyboard
-    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    val inputMethodManager =
+      getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
   }
 
   private fun updateNickname(view: View) {
-    val editText = findViewById<EditText>(R.id.nickname_edit)
-    val doneButton = findViewById<Button>(R.id.done_button)
-    editText.visibility = View.VISIBLE
-    doneButton.visibility = View.VISIBLE
-    view.visibility = View.GONE
-    editText.requestFocus()
-    // Show the keyboard.
-    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.showSoftInput(editText, 0)
+    binding.apply {
+      nicknameEdit.visibility = View.VISIBLE
+      doneButton.visibility = View.VISIBLE
+      view.visibility = View.GONE
+      nicknameEdit.requestFocus()
+      // Show the keyboard.
+      val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+      imm.showSoftInput(nicknameEdit, 0)
+    }
   }
 }
---
+++ Kotlin Fundamentals/Lesson2/app/src/main/java/dev/emg/lesson2/MyName.kt
@@ -0,0 +1,9 @@
+package dev.emg.lesson2
+
+/**
+ * Created by emeruvia on 7/29/2019.
+ */
+data class MyName(
+  var name: String = "",
+  var nickname: String = ""
+)
