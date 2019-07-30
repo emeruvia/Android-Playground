886e171034e9a4a50dc14bf26b675154cbb49c60
Edgar Meruvia <emeruvia@gmail.com>
2019-07-30 00:17:21 -0400

    Finished lesson 3.1

--- Kotlin Fundamentals/Lesson3/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Kotlin Fundamentals/Lesson3/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -22,19 +22,9 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-    }
-
-    // TODO (01) Create the new TitleFragment
-    // Select File->New->Fragment->Fragment (Blank)
-
-    // TODO (02) Clean up the new TitleFragment
-    // In our new TitleFragment
-
-    // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
-    // In our new TitleFragment
-    // R.layout.fragment_title
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    @Suppress("UNUSED_VARIABLE")
+    val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+  }
 }
