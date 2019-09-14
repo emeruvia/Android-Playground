312b7d9f1dd04efefcfce4b7c7ab98d713d94f76
Edgar Meruvia <emeruvia@gmail.com>
2019-09-14 18:06:55 -0400

    use @contributesAndroidInjector for injecting activities to application

--- Dagger/app/src/main/java/com/emgdev/daggerpractice/AuthActivity.kt
+++ Dagger/app/src/main/java/com/emgdev/daggerpractice/AuthActivity.kt
@@ -2,11 +2,18 @@ package com.emgdev.daggerpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : DaggerAppCompatActivity() {
+
+  @Inject lateinit var testString: String
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_auth)
+
+    Log.d("AuthActivity()", testString)
   }
 }
--- Dagger/app/src/main/java/com/emgdev/daggerpractice/di/ActivityBuilderModule.kt
+++ Dagger/app/src/main/java/com/emgdev/daggerpractice/di/ActivityBuilderModule.kt
@@ -2,11 +2,19 @@ package com.emgdev.daggerpractice.di
 
 import com.emgdev.daggerpractice.AuthActivity
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 
-@Module
-abstract class ActivityBuilderModule {
+@Module(includes = [ApplicationModuleBinds::class])
+object ActivityBuilderModule {
 
+  @JvmStatic
+  @Provides
+  internal fun someString(): String = "this is a test string"
+}
+
+@Module
+abstract class ApplicationModuleBinds {
   @ContributesAndroidInjector
-  abstract fun contributeAuthActivity(): AuthActivity
+  internal abstract fun contributeAuthActivity(): AuthActivity
 }
