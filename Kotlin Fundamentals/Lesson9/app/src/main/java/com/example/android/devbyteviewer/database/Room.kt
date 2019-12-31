d441e3a746e5c7edd762f4cd3f5182cbcfb91200
Edgar Meruvia <emg.appdev@gmail.com>
2019-12-30 23:05:27 -0500

    Finish lesson 9.1: Repository

--- Kotlin Fundamentals/Lesson9/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ Kotlin Fundamentals/Lesson9/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -15,3 +15,48 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Dao
+interface VideoDao {
+
+  @Query("SELECT * FROM databasevideo")
+  fun getVideos(): LiveData<List<DatabaseVideo>>
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun insertAll(videos: List<DatabaseVideo>)
+
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+  abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+  synchronized(VideosDatabase::class.java) {
+    // isInitialized checks if the lateinit var has been initialized
+    if (!::INSTANCE.isInitialized) {
+      INSTANCE =
+        Room.databaseBuilder(
+            context.applicationContext,
+            VideosDatabase::class.java,
+            "videos"
+        )
+            .build()
+    }
+  }
+  return INSTANCE
+}
+
--- Kotlin Fundamentals/Lesson9/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ Kotlin Fundamentals/Lesson9/app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.squareup.moshi.JsonClass
 
@@ -58,6 +59,18 @@ data class NetworkVideo(
 fun NetworkVideoContainer.asDomainModel(): List<DevByteVideo> {
   return videos.map {
     DevByteVideo(
+        title = it.title,
+        description = it.description,
+        url = it.url,
+        updated = it.updated,
+        thumbnail = it.thumbnail
+    )
+  }
+}
+
+fun NetworkVideoContainer.asDatabaseModel(): List<DatabaseVideo> {
+  return videos.map {
+    DatabaseVideo(
         title = it.title,
         description = it.description,
         url = it.url,
