b2ab2c0a94f68d7df4bf31bd4b5b4ef3e9377d6b
Edgar Meruvia <emg.appdev@gmail.com>
2020-01-30 00:49:27 -0500

    Implemented: - AppComponent. Factory for passing context and inject for RegistrationActivity - StorageModule for binding Storagee -> SharedPreferencesStorage - SharedPreferencesStorage. Constructor inject for context

---
+++ Android-Dagger-Codelab/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,20 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
+import dagger.Component
+
+@Component(modules = [StorageModule::class])
+interface AppComponent {
+
+    // Factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        // With @BindInstance, the Context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    // Classes that can be injected by this component
+    fun inject(activity: RegistrationActivity)
+}
