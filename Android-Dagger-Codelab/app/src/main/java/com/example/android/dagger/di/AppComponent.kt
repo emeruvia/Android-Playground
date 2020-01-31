f55804a0dccafa89ab7d580f58108ca61db3369d
Edgar Meruvia <emg.appdev@gmail.com>
2020-01-31 00:19:00 -0500

    Add @Singleton scopes for AppComponent and UserManager class

--- Android-Dagger-Codelab/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ Android-Dagger-Codelab/app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -5,7 +5,9 @@ import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [StorageModule::class])
 interface AppComponent {
 
