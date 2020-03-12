3f6dd002b416d35cbd3fd0f7e19f8dda6817c6d8
Edgar Meruvia <emg.appdev@gmail.com>
2020-03-12 15:41:42 -0400

    Starting point for powerful android app project

--- Powerful-Android-Apps/app/src/main/java/dev/emg/powerfulandroid/AuthActivity.kt
+++ Powerful-Android-Apps/app/src/main/java/dev/emg/powerfulandroid/AuthActivity.kt
@@ -1,12 +1,12 @@
 package dev.emg.powerfulandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_auth)
   }
 }
