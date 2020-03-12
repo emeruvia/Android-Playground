c47c91cd0755dc4d7449eadb7332913416f3c82b
Edgar Meruvia <emg.appdev@gmail.com>
2020-03-12 19:35:42 -0400

    Initial implementation of navigation graph

--- Powerful-Android-Apps/app/src/main/java/dev/emg/powerfulandroid/ui/auth/AuthActivity.kt
+++ Powerful-Android-Apps/app/src/main/java/dev/emg/powerfulandroid/ui/auth/AuthActivity.kt
@@ -1,12 +1,13 @@
-package dev.emg.powerfulandroid
+package dev.emg.powerfulandroid.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.emg.powerfulandroid.R.layout
 
 class AuthActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_auth)
+    setContentView(layout.activity_auth)
   }
 }
---
+++ Powerful-Android-Apps/app/src/main/java/dev/emg/powerfulandroid/ui/auth/ForgotPasswordFragment.kt
@@ -0,0 +1,20 @@
+package dev.emg.powerfulandroid.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dev.emg.powerfulandroid.R
+
+class ForgotPasswordFragment : Fragment() {
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+  }
+
+}
